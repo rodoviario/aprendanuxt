9fddabf503c2df9a8c861d5843aa0c1c5acce86e
rodoviario <rodochile@hotmail.com>
2017-08-01 08:01:49 -0300

    Custom-Routes

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,44 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        NUXT
-      </h1>
-      <h2 class="subtitle">
-        Universal Vue.js Application
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div class="container">
+    <h2>Users</h2>
+    <ul class="users">
+      <li v-for="user in users">
+        <nuxt-link :to="'/users/'+user.id">{{ user.name }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import axios from 'axios'
 
 export default {
-  components: {
-    Logo
+  async asyncData () {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+    return { users: data }
   }
 }
 </script>
 
-<style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+<style scoped>
+.container {
   text-align: center;
+  margin-top: 100px;
+  font-family: sans-serif;
 }
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.users {
+  list-style-type: none;
 }
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.users li a {
+  display: inline-block;
+  width: 200px;
+  border: 1px #ddd solid;
+  padding: 10px;
+  text-align: left;
+  color: #222;
+  text-decoration: none;
 }
-.links
-{
-  padding-top: 15px;
+.users li a:hover {
+  color: orange;
 }
 </style>
